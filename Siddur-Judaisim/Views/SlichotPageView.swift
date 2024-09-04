fae2723375a0aee455780b06e22268da7aa2a6b4
YardenDalii <yarden.dali11@gmail.com>
2024-09-04 13:29:07 +0300

    Slichot prayers

---
+++ Siddur-Judaisim/Views/SlichotPageView.swift
@@ -0,0 +1,18 @@
+//
+//  SlichotPageView.swift
+//  Siddur-Judaisim
+//
+//  Created by Yarden Dali on 04/09/2024.
+//
+
+import SwiftUI
+
+struct SlichotPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SlichotPageView()
+}
